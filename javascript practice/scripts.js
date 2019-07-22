580316d4a1ee1632906a71cd9644ecbecf7939f3
Florin Holhos <florin.holhos@softvision.ro>
2019-07-22 17:59:25 +0300

    Finished form

--- javascript practice/scripts.js
+++ javascript practice/scripts.js
@@ -17,20 +17,20 @@
 
     // firstName
     firstName.addEventListener('change', function(event){
-        var value = document.getElementById("firstName").value;
-        if (value == "") {
-            document.getElementById("firstName").setAttribute("style", "border: 1px solid red;");
-        } else {
-            document.getElementById("firstName").setAttribute("style", "border: 1px solid #777777;");
-        }
-
         // if the 3 fields aren't empty enable checkout button
         let fname = checkName(firstName.value);
         let lname = checkName(lastName.value);
         let mail = validateEmail(email.value);
 
+        if (firstName.value === "" || !fname) {
+            firstName.setAttribute("style", "border: 1px solid red;");
+        } else {
+            firstName.setAttribute("style", "border: 1px solid #777777;");
+        }
+
         if(fname && lname && mail === true){
             checkoutBtn.disabled = false;
+            checkoutBtn.style.cursor = "pointer";
             document.getElementById('custom-error').style.display = "none";
         } else {
             document.getElementById('custom-error').style.display = "block";
@@ -39,20 +39,21 @@
 
     // lastName
     lastName.addEventListener('change', function(event){
-        var value = document.getElementById("lastName").value;
-        if (value == "") {
-            document.getElementById("lastName").setAttribute("style", "border: 1px solid red;");
-        } else {
-            document.getElementById("lastName").setAttribute("style", "border: 1px solid #777777;");
-        }
 
         // if the 3 fields aren't empty enable checkout button
         let fname = checkName(firstName.value);
         let lname = checkName(lastName.value);
         let mail = validateEmail(email.value);
 
+        if (lastName.value === "" || !lname) {
+            lastName.setAttribute("style", "border: 1px solid red;");
+        } else {
+            lastName.setAttribute("style", "border: 1px solid #777777;");
+        }
+
         if(fname && lname && mail === true){
             checkoutBtn.disabled = false;
+            checkoutBtn.style.cursor = "pointer";
             document.getElementById('custom-error').style.display = "none";
         } else {
             document.getElementById('custom-error').style.display = "block";
@@ -61,20 +62,21 @@
 
     // email 
     email.addEventListener('change', function(event){
-        var email = document.getElementById("email").value;
-        if (email == "") {
-            document.getElementById("email").setAttribute("style", "border: 1px solid red;");
-        } else {
-            document.getElementById("email").setAttribute("style", "border: 1px solid #777777;");
-        }
-
         // if the 3 fields aren't empty enable checkout button
         let fname = checkName(firstName.value);
         let lname = checkName(lastName.value);
-        let mail = validateEmail(email);
+        let mail = validateEmail(email.value);
+
+        if (email.value === "" || !mail) {
+            email.setAttribute("style", "border: 1px solid #fc031c;");
+            email.style.color = "#fc031c";
+        } else {
+            email.setAttribute("style", "border: 1px solid #777777;");
+        }
 
         if(fname && lname && mail) {
             checkoutBtn.disabled = false;
+            checkoutBtn.style.cursor = "pointer";
             document.getElementById('custom-error').style.display = "none";
         } else {
             document.getElementById('custom-error').style.display = "block";
@@ -89,20 +91,62 @@
 
     // products actions
     // remove item
-    let removeBtn = document.getElementsByClassName('remove-item');
-    let myFunction = function(event) {
+    const removeBtn = document.getElementsByClassName('remove-item');
+    const updateRow = function(event) {
         event.preventDefault();
         let target = event.target;
         let targetPrice = target.parentElement.parentElement.children[4].innerHTML;
         deleteRow(target);
-        updatePrice(targetPrice);
-        
+        decreaseTotal(Number(targetPrice));
     };
 
     // add click events for remove buttons
     for (let i = 0; i < removeBtn.length; i++) {
-        removeBtn[i].addEventListener('click', myFunction, false);
+        removeBtn[i].addEventListener('click', updateRow, false);
     }
+
+    // decrease quantity
+    const decreaseBtn = document.getElementsByClassName('remove');
+    const decreaseQuantity = function(event) {
+        event.preventDefault();
+        let target = event.target;
+        let quantity = target.parentElement.children[1];
+        let unitPrice = target.parentElement.parentElement.children[2].innerHTML;
+        let totalPrice = target.parentElement.parentElement.children[4];
+
+        if(quantity.innerHTML > 1) {
+            quantity.innerHTML = quantity.innerHTML - 1;
+            totalPrice.innerHTML = Number(unitPrice) * Number(quantity.innerHTML);
+            decreaseTotal(Number(unitPrice));
+        } else {
+            return;
+        }
+    }
+
+    // add click events for - buttons
+    for(let i = 0; i < decreaseBtn.length; i++){
+        decreaseBtn[i].addEventListener('click', decreaseQuantity, false);
+    }
+
+    // increase quantity
+    const increaseBtn = document.getElementsByClassName('add');
+    const increaseQuantity = function(event) {
+        event.preventDefault();
+        let target = event.target;
+        let quantity = target.parentElement.children[1];
+        let unitPrice = target.parentElement.parentElement.children[2].innerHTML;
+        let totalPrice = target.parentElement.parentElement.children[4];
+        quantity.innerHTML = Number(quantity.innerHTML) + 1 ;
+        totalPrice.innerHTML = Number(unitPrice) * Number(quantity.innerHTML);
+        increaseTotal(unitPrice);
+    }
+    
+    // add click events for + buttons
+    for(let i = 0; i < increaseBtn.length; i++){
+        increaseBtn[i].addEventListener('click', increaseQuantity, false);
+    }
+
+    
 
 })();
 /********************* END MAIN FUNCTION **********************/
@@ -110,8 +154,8 @@
 
 /********** HELP FUNCTIONS **********/
 function checkName(name) {
-    let letters = "^[a-zA-Z]*$";
-    if (name.length > 4 && name.match(letters)){
+    let letters = /^[A-Za-z ]+$/;
+    if (name.match(letters)){
         return true;
     } else {
         return false;
@@ -122,11 +166,21 @@ function validateEmail(email) {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
 }
 
-const deleteRow = (target) => {
+const deleteRow = function (target) {
     target.parentElement.parentElement.style.display = "none";
 }
 
-const updatePrice = (targetPrice) => {
+const increaseTotal = function(targetPrice) {
     let total = document.getElementsByClassName('total')[0];
-    total.innerHTML = total.innerHTML - targetPrice;
+    updateTotal(Number(total.innerHTML) + Number(targetPrice));
+}
+
+const decreaseTotal = function(targetPrice) {
+    let total = document.getElementsByClassName('total')[0];
+    updateTotal(Number(total.innerHTML) - Number(targetPrice));
+}
+
+const updateTotal = function(price) {
+    let total = document.getElementsByClassName('total')[0];
+    total.innerHTML = Number(price);
 }
